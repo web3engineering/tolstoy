e6a5432dcfa638de6b72f518e8af52a69a9280c1
KOSTYAN <UNORDERED_SET@TUTANOTA.COM>
2022-12-19 13:54:58 +0000

    Metrics + Queries

--- main.go
+++ main.go
@@ -10,6 +10,8 @@ import (
 	"math/big"
 	"time"
 	"context"
+	"sync/atomic"
+	"strconv"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/ethereum/go-ethereum"
@@ -18,6 +20,12 @@ import (
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 )
+
+var QUERY_RANGE uint64 = 50
+var SCAN_DELAY uint64 = 20  // To deal with reords in a lazy manner we are just not fetching 20 latest blocks
+
+var reconnect_attempts_metrics uint32 = 0
+var last_block_metrics uint64 = 0
 
 
 func reconnect(rpc_index *int) (bind.ContractBackend) {
@@ -34,6 +42,7 @@ func reconnect(rpc_index *int) (bind.ContractBackend) {
 			log.Printf("Failed to connect to blockhain:", err)
 			time.Sleep(time.Duration(30)*time.Second)
 			*rpc_index = *rpc_index + 1
+			atomic.AddUint32(&reconnect_attempts_metrics, 1)
 			continue
 		}
 		log.Printf("Successfully connected to RPC %v", rpc_url)
@@ -91,10 +100,20 @@ func runScanner(db *sql.DB) {
 	poolReferralPaymentSigantureHash := crypto.Keccak256Hash(poolReferralPaymentSiganture)
 	log.Printf("poolReferralPaymentSigantureHash %v", poolReferralPaymentSigantureHash.Hex())
 
+	var last_block_from_db uint64 = 0
+	log.Printf("Getting last block from DB...")
+	err = db.QueryRow("SELECT last_block FROM system_status WHERE r=1").Scan(&last_block_from_db)
+	if err != nil {
+		log.Printf("ERROR: unable to get last block from DB %v", err)
+	}
+	if last_block_from_db == 0 {
+		log.Fatal("ERROR: last block from DB is missing")
+	}
+
 	opts := new(bind.FilterOpts)
-	opts.Start = 29367000
+	opts.Start = last_block_from_db
 	opts.End = new(uint64)
-	*opts.End = 29367050
+	*opts.End = last_block_from_db + QUERY_RANGE
 
 	for {
 		log.Printf("Querying blockchain for events in %v:%v range", opts.Start, *opts.End)
@@ -103,12 +122,18 @@ func runScanner(db *sql.DB) {
 			FromBlock: big.NewInt(int64(opts.Start)),
 			ToBlock:   big.NewInt(int64(*opts.End)),
 			Addresses: []common.Address{
-				poolContractAddress, slotsContractAddress, coinflipContractAddress,
+				poolContractAddress,
+				slotsContractAddress,
 				rouletteContractAddress,
+				coinflipContractAddress,
 			},
-			Topics: [][]common.Hash{
-				{slotsGameResEventSignatureHash, rouletteGameResEventSignatureHash},
-			},
+			Topics: [][]common.Hash{{
+				poolSharePriceChangedEventSigantureHash,
+				poolReferralPaymentSigantureHash,
+				slotsGameResEventSignatureHash,
+				rouletteGameResEventSignatureHash,
+				coinflipGameResEventSignatureHash,
+			}},
 		}
 
 		rawLogs, err := client.FilterLogs(context.Background(), query)
@@ -131,10 +156,10 @@ func runScanner(db *sql.DB) {
 		
 				log.Printf("Found Slots Game Res %v", rawLog)
 				_, err = db.Exec("INSERT IGNORE INTO recent_games(tx, block_number, game_type, player, amount) VALUES (?, ?, 'SLOT', ?, ?)",
-					rawLog.TxHash.String(),
+					fmt.Sprintf("%s:%d", rawLog.TxHash.String(), rawLog.Index),
 					rawLog.BlockNumber,
 					slotGameResLog.UserAddress.Hex(),
-					big.NewInt(0).Div(slotGameResLog.WinAmount, big.NewInt(0x100000000)).Uint64());
+					big.NewInt(0).Div(slotGameResLog.WinAmount, big.NewInt(1000000000)).Uint64());
 	
 				if err != nil {
 					log.Fatal("Unable to insert to DB: %v", err)
@@ -149,12 +174,11 @@ func runScanner(db *sql.DB) {
 				}
 
 				log.Printf("Found Roulette Game Res %v", rawLog)
-	
 				_, err = db.Exec("INSERT IGNORE INTO recent_games(tx, block_number, game_type, player, amount) VALUES (?, ?, 'ROULETTE', ?, ?)",
-					rawLog.TxHash.String(),
+					fmt.Sprintf("%s:%d", rawLog.TxHash.String(), rawLog.Index),
 					rawLog.BlockNumber,
 					rouletteGameResLog.UserAddress.Hex(),
-					big.NewInt(0).Div(rouletteGameResLog.WinAmount, big.NewInt(0x100000000)).Uint64());
+					big.NewInt(0).Div(rouletteGameResLog.WinAmount, big.NewInt(1000000000)).Uint64());
 	
 				if err != nil {
 					log.Fatal("Unable to insert to DB: %v", err)
@@ -168,11 +192,12 @@ func runScanner(db *sql.DB) {
 					log.Fatalf("Unable to parse coinflip::GameResEvent %v", err)
 				}
 		
+				log.Printf("Found Coinflip Game Res %v", rawLog)
 				_, err = db.Exec("INSERT IGNORE INTO recent_games(tx, block_number, game_type, player, amount) VALUES (?, ?, 'COINFLIP', ?, ?)",
-					rawLog.TxHash.String(),
+					fmt.Sprintf("%s:%d", rawLog.TxHash.String(), rawLog.Index),
 					rawLog.BlockNumber,
 					coinflipGameResLog.UserAddress.Hex(),
-					big.NewInt(0).Div(coinflipGameResLog.WinAmount, big.NewInt(0x100000000)).Uint64());
+					big.NewInt(0).Div(coinflipGameResLog.WinAmount, big.NewInt(1000000000)).Uint64());
 	
 				if err != nil {
 					log.Fatal("Unable to insert to DB: %v", err)
@@ -186,11 +211,12 @@ func runScanner(db *sql.DB) {
 					log.Fatalf("Unable to parse poolContract::SharePriceChanged %v", err)
 				}
 		
+				log.Printf("Found PnL change event %v", rawLog)
 				_, err = db.Exec("INSERT IGNORE INTO pnl_changes (tx, block_number, stored_eth, total_shares) VALUES (?, ?, ?, ?)",
 					rawLog.TxHash.String(),
 					rawLog.BlockNumber,
-					big.NewInt(0).Div(poolContractSharePriceChangedResLog.NewPriceNom, big.NewInt(0x100000000)).Uint64(),
-					big.NewInt(0).Div(poolContractSharePriceChangedResLog.NewPriceDenom, big.NewInt(0x100000000)).Uint64());
+					big.NewInt(0).Div(poolContractSharePriceChangedResLog.NewPriceNom, big.NewInt(1000000000)).Uint64(),
+					big.NewInt(0).Div(poolContractSharePriceChangedResLog.NewPriceDenom, big.NewInt(1000000000)).Uint64());
 
 				if err != nil {
 					log.Fatal("Unable to insert to DB: %v", err)
@@ -204,17 +230,24 @@ func runScanner(db *sql.DB) {
 					log.Fatalf("Unable to parse poolContract::ReferralPayment %v", err)
 				}
 		
+				log.Printf("Found Ref Payment Event %v", rawLog)
 				_, err = db.Exec("INSERT IGNORE INTO ref_payments (tx, block_number, ref, payment_received) VALUES (?, ?, ?, ?)",
-					rawLog.TxHash.String(),
+					fmt.Sprintf("%s:%d", rawLog.TxHash.String(), rawLog.Index),
 					rawLog.BlockNumber,
 					poolContractReferralPaymentLog.Referrer.Hex(),
-					big.NewInt(0).Div(poolContractReferralPaymentLog.ReferralPaymentAmount, big.NewInt(0x100000000)).Uint64());
+					big.NewInt(0).Div(poolContractReferralPaymentLog.ReferralPaymentAmount, big.NewInt(1000000000)).Uint64());
 
 				if err != nil {
 					log.Fatal("Unable to insert to DB: %v", err)
 				}
 				continue
 			}
+		}
+		
+		atomic.StoreUint64(&last_block_metrics, *opts.End)
+		_, err = db.Exec("UPDATE system_status SET last_block = ? WHERE r=1 AND last_block < ?", last_block_metrics, last_block_metrics)
+		if err != nil {
+			log.Fatal("Unable to update system_status DB: %v", err)
 		}
 
 		for {
@@ -226,14 +259,14 @@ func runScanner(db *sql.DB) {
 				client = reconnect(&rpc_index)
 			}
 			log.Printf("The most recent block is %v", header.Number)
-			if header.Number.Uint64() < *opts.End + 50 + 20 {
+			if header.Number.Uint64() < *opts.End + QUERY_RANGE + SCAN_DELAY {
 				time.Sleep(time.Duration(120)*time.Second)
 			} else {
 				break
 			}
 		}
 		opts.Start = *opts.End
-		*opts.End = *opts.End + 50
+		*opts.End = *opts.End + QUERY_RANGE
 	}
 
 	log.Printf("No new events")
@@ -273,12 +306,36 @@ func ensureDatabases(db *sql.DB) {
 		log.Fatal("Failed to create table:", err)
 	}
 
-	log.Printf("Creating table system status")
+	log.Printf("Creating table system_status")
 	_, err = db.Exec(`CREATE TABLE IF NOT EXISTS system_status(
 		last_block BIGINT NOT NULL
 	)`)
 	if err != nil {
 		log.Fatal("Failed to create table:", err)
+	}
+
+	var system_status_has_rowid uint32 = 0
+	err = db.QueryRow("SELECT count(*) FROM information_schema.COLUMNS WHERE TABLE_SCHEMA = 'pnl' AND TABLE_NAME = 'system_status' AND COLUMN_NAME = 'r'").Scan(&system_status_has_rowid)
+	if err != nil {
+		log.Fatalf("Unable to check if column 'r' exists for system_status migration: %v", err)
+	}
+	if system_status_has_rowid == 0 {
+		log.Printf("Migration for system_status: add column 'r'")
+		_, err = db.Exec(`ALTER TABLE system_status ADD COLUMN r INT NOT NULL UNIQUE`)
+		if err != nil {
+			log.Fatal("Failed to Migrate table:", err)
+		}
+	} else {
+		log.Print("Already Done: Migration for system_status: add column 'r'")
+	}
+
+	last_block_from_env, err := strconv.ParseUint(os.Getenv("FIRST_BLOCK"), 10, 64)
+	if err != nil {
+		log.Fatal("FIRST_BLOCK env variable is missing!")
+	}
+	_, err = db.Exec("INSERT IGNORE INTO system_status(last_block, r) VALUES (?, 1)", last_block_from_env)
+	if err != nil {
+		log.Fatalf("Unable to write last_block info system_status: %v", err)
 	}
 }
 
@@ -328,6 +385,7 @@ func (s *Service) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
+		defer rows.Close()
 
 		for rows.Next() {
 			var entry PnlEntry
@@ -370,18 +428,26 @@ func (s *Service) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 			Data []ReferralPaymentEntry `json:"data"`
 		};
 		var resp Response
+		var rows *sql.Rows
+		var err error
 
-		rows, err := db.Query("select ref, SUM(payment_received) as amount from ref_payments group by ref;")
+		referral, hasReferral := r.URL.Query()["referral"]
+		if !hasReferral {
+			rows, err = db.Query("select ref, SUM(payment_received) as amount from ref_payments group by ref;")
+		} else {
+			rows, err = db.Query("select ref, SUM(payment_received) as amount from ref_payments where LOWER(ref) = LOWER(?) group by ref", referral[0])
+		}
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
+		defer rows.Close()
 
 		for rows.Next() {
 			var entry ReferralPaymentEntry
 			var amount uint64
-			err = rows.Scan(&entry.Address, &amount)
-			entry.Amount = float64(amount) / 1e10
+			err := rows.Scan(&entry.Address, &amount)
+			entry.Amount = float64(amount) / 1e9
 			if err != nil {
 				break
 			}
@@ -422,16 +488,27 @@ func (s *Service) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 			Data []RecentGameEntry `json:"data"`
 		};
 		var resp Response
+		var rows *sql.Rows
+		var err error
 
-		rows, err := db.Query("select block_number, game_type, player, amount from recent_games order by block_number desc limit 25;")
+		gameType, hasGameType := r.URL.Query()["game-type"]
+		if !hasGameType {
+			rows, err = db.Query("select block_number, game_type, player, amount from recent_games order by block_number desc limit 25;")
+		} else {
+			rows, err = db.Query("select block_number, game_type, player, amount from recent_games WHERE game_type = ? order by block_number desc limit 25;", gameType[0])
+		}
+
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
+		defer rows.Close()
 
 		for rows.Next() {
 			var entry RecentGameEntry
-			err = rows.Scan(&entry.BlockNumber, &entry.GameType, &entry.Address, &entry.WinAmount)
+			var amount float64
+			err = rows.Scan(&entry.BlockNumber, &entry.GameType, &entry.Address, &amount)
+			entry.WinAmount = amount / 1e9
 			if err != nil {
 				break
 			}
@@ -459,6 +536,14 @@ func (s *Service) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 		json.NewEncoder(w).Encode(resp)
+		return
+
+	case "/metrics":
+		fmt.Fprintf(w, "# TYPE last_block gauge\n")
+		fmt.Fprintf(w, "last_block %d\n", atomic.LoadUint64(&last_block_metrics))
+
+		fmt.Fprintf(w, "# TYPE total_reconnects counter\n")
+		fmt.Fprintf(w, "total_reconnects %d\n", atomic.LoadUint32(&reconnect_attempts_metrics))
 		return
 	}
 }
